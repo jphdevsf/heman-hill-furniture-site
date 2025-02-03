0d13c2bbf824a59c3b61ff525ea41f72fb11d9f6
jphdevsf <62826600+jphdevsf@users.noreply.github.com>
2025-02-02 16:32:49 -0800

    header.module.css reference

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import styles from './Header.module.css';
+import styles from './header.module.css';
 
 export default function Header() {
   return (
