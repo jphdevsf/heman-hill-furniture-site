e71641f8ed2b42e89c26d85170cbe8e8481b3edb
jphdevsf <62826600+jphdevsf@users.noreply.github.com>
2025-02-04 22:47:43 -0800

    update About section with proofread edits

--- src/app/components/About.jsx
+++ src/app/components/About.jsx
@@ -18,10 +18,10 @@ const About = () => {
           <h2 className="text-4xl md:text-5xl mb-8 font-jost font-light">About</h2>
           <span className="text-left font-light text-lg/8 md:text-xl/10 mb-4 inline-block mx-2 md:ml-8">
           <p className="mb-8">
-              HemanHill manufacturers two lines of furniture that are designed to create a unique style for your gallery. The French Country and Italian Country lines are made from old Barnwood. The Live Edge line is made from reclaimed trees, both lines are environmentally accurate, finished in water based paint and polyurethane. The hardware is hand made by a blacksmith in Santa Barbara.
+          HemanHill manufactures two lines of furniture that are designed to create a unique style for your gallery. The French Country and Italian Country lines are made from old barnwood. The Live Edge line is made from reclaimed trees. Both lines are environmentally friendly, finished in water-based paint and polyurethane. The hardware is handmade by a blacksmith in Santa Barbara.
           </p>
           <p className="mb-8">
-              HemanHill started working with wood at the age of 5. His Godfather, Jim Godsey, was a 4th generation grandfather clock maker who had his shop next to Heman's house. At age 12 he started to make guitars and after college in Seattle he studied for two years with the Smithsonian artist Sodergren. The Hemanhill catalog was started in 1995.
+          HemanHill started working with wood at the age of 5. His godfather, Jim Godsey, was a 4th generation grandfather clock maker who had his shop next to Heman's house. At age 12, he started to make guitars, and after college in Seattle, he studied for two years with the Smithsonian artist Sodergren. The HemanHill catalog was started in 1995.
           </p>
           </span>
         </div>
