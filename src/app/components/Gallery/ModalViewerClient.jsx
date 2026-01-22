b531c30ced4c58710ae56ef8e9133328286e98ca
jphdevsf <62826600+jphdevsf@users.noreply.github.com>
2026-01-21 19:30:51 -0800

    add tailwind classes to modal elements to hide blue focus line on mobile devices, still allow keyboard focus line

--- src/app/components/Gallery/ModalViewerClient.jsx
+++ src/app/components/Gallery/ModalViewerClient.jsx
@@ -36,12 +36,12 @@ const ModalViewerClient = ({ image, onClose }) => {
         onRequestClose={onClose}
         shouldCloseOnOverlayClick={true}
         preventScroll={true}
-        className="inline-block mx-auto"
+        className="inline-block mx-auto focus:outline-none"
         overlayClassName="overlay fixed flex items-start justify-center inset-0 bg-black bg-opacity-80 z-50 text-center overflow-auto"
       >
         <button
           type="button"
-          className="fixed top-2.5 right-2.5 text-white bg-black w-8 h-8 flex items-center justify-center"
+          className="fixed top-2.5 right-2.5 text-white bg-black w-8 h-8 flex items-center justify-center focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-50 z-20"
           onClick={onClose}
         >
           <svg
@@ -73,7 +73,16 @@ const ModalViewerClient = ({ image, onClose }) => {
         </button>
 
         {image && (
-          <button type="button" className="m-4 relative" onClick={onClose}>
+          <button
+            type="button"
+            className="m-4 relative focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-50 z-10"
+            onClick={onClose}
+          >
+            {alt.length > 0 && (
+              <p className="md:absolute left-0 top-0 mt-4 md:mt-0 p-4 opacity bg-black/90 font-light text-white">
+                {alt}
+              </p>
+            )}
             <Image
               src={url}
               alt={alt}
@@ -82,9 +91,6 @@ const ModalViewerClient = ({ image, onClose }) => {
               loading="eager"
               className="block max-w-full h-auto"
             />
-            {alt.length > 0 && (
-              <p className="absolute left-0 top-0 p-4 bg-opacity-80 bg-black text-white">{alt}</p>
-            )}
           </button>
         )}
       </Modal>
